68986c6e494623aae7c46303a6fd3eedce10e8ac
saminaishfaq <79367483+saminaishfaq@users.noreply.github.com>
2021-08-28 02:39:22 -0700

    backend code

---
+++ src/model.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+//MongoDB schema
+const userSchema = mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50,
+  },
+   userName:{
+     type:String,
+     required:true,
+     minlength:5,
+     maxlength: 50,
+   },
+  email: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 1024,
+  },
+});
+
+const UserModal = mongoose.model("User", userSchema);
+
+module.exports = UserModal;
